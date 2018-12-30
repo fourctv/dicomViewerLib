f60c8b3d9a34235db9d9f99d0b6ba9fde717e80a
fourctv <julio@4ctv.com>
2018-12-30 20:14:44 -0200

    remove BrowserModule from library declaration

--- projects/dicom-viewer/src/lib/dicom-viewer.module.ts
+++ projects/dicom-viewer/src/lib/dicom-viewer.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -11,7 +10,6 @@ import { ThumbnailDirective } from './thumbnail.directive';
 
 @NgModule({
   imports: [
-    BrowserModule,
     FormsModule,
     CommonModule,
     MatProgressSpinnerModule
