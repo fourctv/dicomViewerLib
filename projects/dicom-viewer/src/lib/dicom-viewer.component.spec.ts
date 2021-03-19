9c8365bafd94c21b0ea9cfae0e4e52e135c94e46
rmdern <ryanmdern@gmail.com>
2021-03-19 01:54:38 -0400

    ng updates

--- projects/dicom-viewer/src/lib/dicom-viewer.component.spec.ts
+++ projects/dicom-viewer/src/lib/dicom-viewer.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DICOMViewerComponent } from './dicom-viewer.component';
 
@@ -6,7 +6,7 @@ describe('DICOMViewerComponent', () => {
   let component: DICOMViewerComponent;
   let fixture: ComponentFixture<DICOMViewerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DICOMViewerComponent ]
     })
